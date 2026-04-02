94269b402e93963a4f4cbd32643511bc49c8a6b7
nikitos <bochkonickita@yandex.ru>
2026-04-02 10:01:29 +0300

    ПРОВЕРКА

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { Routes, Route } from "react-router-dom";
 import Editor from "./features/editor/components/Editor/Editor.tsx";
+import MainPage from "./features/main/components/MainPage/MainPage.tsx";
 function App() {
 
     return <div className="App">
         <Routes>
             <Route path="/editor" element={<Editor />} />
+            <Route path="/" element={<MainPage />} />
+
         </Routes>    </div>
 
 }
