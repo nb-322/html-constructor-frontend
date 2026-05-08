4b174523374f39842be4c8943abe9c936aa4036e
Deidata4554 <vova.kuznetsov332@gmail.com>
2026-05-08 19:30:21 +0300

    fix: изменение названия ролей

--- backend/migrations/000003_create_users_table.up.sql
+++ backend/migrations/000003_create_users_table.up.sql
@@ -1,9 +1,9 @@
 -- 000003_create_users_table.up.sql
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    login VARCHAR(100),
+    login VARCHAR(100) NOT NULL,
     password_hash VARCHAR(255),
-    role VARCHAR(20),
+    role VARCHAR(20) NOT NULL  CHECK (role IN ('админ', 'маркетолог', 'аналитик')),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     is_deleted BOOLEAN DEFAULT FALSE,
     deleted_at TIMESTAMP WITH TIME ZONE NULL,
